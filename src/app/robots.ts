a12cd0c776d5fbf43731e5957e1132c708c27c8a
waeldaas192 <waeeldaas@gmail.com>
2026-09-17 09:26:27 +0100

    feat: make ChatGPT search crawler access explicit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,3 +1,11 @@
 import type { MetadataRoute } from "next";
 export const dynamic = "force-static";
-export default function robots(): MetadataRoute.Robots { return { rules:{ userAgent:"*", allow:"/", disallow:"/api/" }, sitemap:"https://wdmarketing.co.uk/sitemap.xml" }; }
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      { userAgent: "*", allow: "/", disallow: "/api/" },
+      { userAgent: "OAI-SearchBot", allow: "/", disallow: "/api/" },
+    ],
+    sitemap: "https://wdmarketing.co.uk/sitemap.xml",
+  };
+}
